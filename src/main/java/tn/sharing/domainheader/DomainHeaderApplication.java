4ec63dd98599f39bfd94122475b01343dadb5676
candis-D7itinjoeace <ahmedbayrem.bjaoui1@esprit.tn>
2022-10-01 14:35:39 +0200

    hola kubernetes

--- src/main/java/tn/sharing/domainheader/DomainHeaderApplication.java
+++ src/main/java/tn/sharing/domainheader/DomainHeaderApplication.java
@@ -2,14 +2,15 @@ package tn.sharing.domainheader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class DomainHeaderApplication {
 
     public static void main(String[] args) {
